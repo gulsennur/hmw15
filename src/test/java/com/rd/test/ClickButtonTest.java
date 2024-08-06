feac8b49e20c40bd1d9a38eb05e008f77ca3342c
gulsennur <gulsennur1995@gmail.com>
2024-08-06 20:58:39 +0300

    added.

--- src/test/java/com/rd/test/ClickButtonTest.java
+++ src/test/java/com/rd/test/ClickButtonTest.java
@@ -1,2 +1,44 @@
-package com.rd.test;public class ClickButtonTest {
+package com.rd.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+public class ClickButtonTest {
+    private WebDriver driver;
+
+    @BeforeClass
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        driver = new ChromeDriver();
+    }
+
+    @Test
+    public void testClickButton() {
+        driver.get("https://demoqa.com/elements");
+
+        // "Buttons" seçeneğini tıklayın
+        WebElement buttonsOption = driver.findElement(By.xpath("//div[text()='Buttons']/.."));
+        buttonsOption.click();
+
+        // "Click Me" düğmesini tıklayın
+        WebElement clickMeButton = driver.findElement(By.xpath("//button[text()='Click Me']"));
+        clickMeButton.click();
+
+        // Görünen mesajın doğru olup olmadığını kontrol edin
+        WebElement message = driver.findElement(By.id("dynamicClickMessage"));
+        String expectedMessage = "You have done a dynamic click";
+        Assert.assertEquals(message.getText() , expectedMessage);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
 }
--- src/test/java/com/rd/test/testAddRecord.java
+++ src/test/java/com/rd/test/testAddRecord.java
@@ -1,2 +1,37 @@
-package com.rd.test;public class testAddRecord {
+package com.rd.test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class testAddRecord {
+    @Test
+    public void testAddRecord() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://demoqa.com/webtables");
+        driver.findElement(By.xpath("//button[text()='Add']")).click();
+
+        // Add a new record
+        driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("John");
+        driver.findElement(By.xpath("//input[@id='lastName']")).sendKeys("Doe");
+        driver.findElement(By.xpath("//input[@id='userEmail']")).sendKeys("johndoe@example.com");
+        driver.findElement(By.xpath("//input[@id='age']")).sendKeys("30");
+        driver.findElement(By.xpath("//input[@id='salary']")).sendKeys("50000");
+        driver.findElement(By.xpath("//input[@id='department']")).sendKeys("IT");
+        driver.findElement(By.xpath("//button[text()='Submit']")).click();
+
+        // Edit the record
+        driver.findElement(By.xpath("//td[text()='John']//following-sibling::td[1]/span[text()='edit']")).click();
+        driver.findElement(By.xpath("//input[@id='firstName']")).clear();
+        driver.findElement(By.xpath("//input[@id='firstName']")).sendKeys("Jane");
+        driver.findElement(By.xpath("//button[text()='Save']")).click();
+
+        // Verify if record is edited
+        String editedFirstName = driver.findElement(By.xpath("//td[text()=' Jane']")).getText();
+        Assert.assertEquals(editedFirstName, "Jane");
+
+        driver.quit();
+    }
 }
